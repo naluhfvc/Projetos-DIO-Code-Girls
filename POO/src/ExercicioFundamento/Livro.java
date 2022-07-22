56ee6c7712413be20538cba1b1aab44363f8b331
naluhfvc <analufavacho@gmail.com>
2022-07-22 00:03:32 -0300

    Upload

--- POO/src/ExercicioFundamento/Livro.java
+++ POO/src/ExercicioFundamento/Livro.java
@@ -1,4 +1,16 @@
 package ExercicioFundamento;
 
+/*
+Levando em consideração uma loja  on-line de livros,
+modele uma entidade livro.
+ */
 public class Livro {
+
+    String titulo;
+    String isbn;
+    String assunto;
+    int qtdPaginas;
+    String editora;
+    String autor;
+
 }
