997c1e891d9a9708c060774e787ca3025bb237fd
naluhfvc <analufavacho@gmail.com>
2022-07-22 17:22:44 -0300

    Upload

--- POO/src/ExercicioEstruturaBasica/Carro.java
+++ POO/src/ExercicioEstruturaBasica/Carro.java
@@ -1,14 +1,54 @@
 package ExercicioEstruturaBasica;
-
 /*
 Crie a classe "Carro".
  */
 /*
 Evolua o exercicio 1 e defina 3 atributos para sua classe "Carro": cor, modelo e capacidade do tanque.
  */
+/*
+Evolua o conceito do exercício 2 e defina 1 método para calcular o valor total
+ para encher o tanque. Este deve receber como parâmetro o valor da gasolina.
+ Faça também duas sobrecargas do construtor
+ */
 public class Carro {
 
     String cor;
     int capacidadeTanque;
     String modelo;
+
+   /* public Carro (String cor, int capacidadeTanque, String modelo) { //construtor do objeto
+        this.cor = cor;
+        this.modelo = modelo;
+        this.capacidadeTanque = capacidadeTanque;
+    }*/
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public int getCapacidadeTanque() {
+        return capacidadeTanque;
+    }
+
+    public void setCapacidadeTanque(int capacidadeTanque) {
+        this.capacidadeTanque = capacidadeTanque;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public double calcularValorTotal (double valorGasolina){
+
+        return capacidadeTanque * valorGasolina;
+    }
+
 }
---
+++ POO/src/ExercicioEstruturaBasica/Main.java
@@ -0,0 +1,49 @@
+package ExercicioEstruturaBasica;
+/*
+/*
+Evolua o conceito do exercício 3 criando objetos da classe "Carro".
+Use os métodos set/get, quando aplicáveis, para definir os valores dos atributos
+e exibir estes valores "get". Passe também uma mensagem para o cálculo do total
+ pra encher o tanque.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Carro carro1 = new Carro();
+
+        carro1.setModelo("Gol");
+        carro1.setCor("vermelho");
+        carro1.setCapacidadeTanque(50);
+
+        System.out.println("\nCARRO 1:");
+        System.out.println(carro1.getModelo());
+        System.out.println(carro1.getCor());
+        System.out.println(carro1.getCapacidadeTanque());
+        System.out.println(carro1.calcularValorTotal(7.39));
+
+        Carro carro2 = new Carro();
+
+        carro2.setModelo("Uno");
+        carro2.setCor("azul");
+        carro2.setCapacidadeTanque(56);
+
+        System.out.println("\nCARRO 2: ");
+        System.out.println(carro2.getModelo());
+        System.out.println(carro2.getCor());
+        System.out.println(carro2.getCapacidadeTanque());
+        System.out.println(carro2.calcularValorTotal(7.39));
+
+        Carro carro3 = new Carro();
+
+        carro3.setModelo("Civic");
+        carro3.setCor("branco");
+        carro3.setCapacidadeTanque(70);
+
+        System.out.println("\nCARRO 3:");
+        System.out.println(carro3.getModelo());
+        System.out.println(carro3.getCor());
+        System.out.println(carro3.getCapacidadeTanque());
+        System.out.println(carro3.calcularValorTotal(7.39));
+
+    }
+}
