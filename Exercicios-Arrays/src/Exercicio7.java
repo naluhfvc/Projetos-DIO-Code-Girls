9cac5c14eb1342543175f5aad9e01e18b4b6db43
naluhfvc <analufavacho@gmail.com>
2022-07-25 23:55:35 -0300

    Upload

--- Exercicios-Arrays/src/Exercicio7.java
+++ Exercicios-Arrays/src/Exercicio7.java
@@ -3,5 +3,29 @@ Faça um Programa que leia um vetor de 5 números inteiros,
  mostre a soma, a multiplicação e os números.
  */
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Exercicio7 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int[] numeros = new int[5];
+
+        int soma = 0;
+        int multiplicacao = 1;
+
+        for(int i = 0; i< numeros.length; i++){
+            System.out.print("Insira o numero: ");
+             numeros[i] = scan.nextInt();
+
+             soma += numeros[i];
+             multiplicacao *= numeros[i];
+        }
+
+        System.out.println("Numeros: " + Arrays.toString(numeros));
+        System.out.println("Soma: " + soma);
+        System.out.println("Multiplicação : " + multiplicacao);
+
+    }
 }
